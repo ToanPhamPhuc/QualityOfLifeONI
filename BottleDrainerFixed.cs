68e73f0086cc02c2ba3f75c2a76801efc2932383
Pham Phuc Toan <204646057+ToanPhamPhuc@users.noreply.github.com>
2026-07-31 15:51:54 +0700

    Initial commit

---
+++ BottleDrainerFixed.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace QualityOfLifeONI
+{
+    [HarmonyPatch(typeof(LiquidBottlerConfig), nameof(LiquidBottlerConfig.ConfigureBuildingTemplate))]
+    public static class BottleDrainerFixed
+    {
+        public static void Postfix(GameObject go)
+        {
+            Storage storage = go.GetComponent<Storage>();
+            if(storage != null)
+            {
+                storage.SetDefaultStoredItemModifiers(new List<Storage.StoredItemModifier>
+                {
+                    Storage.StoredItemModifier.Hide,
+                    Storage.StoredItemModifier.Seal,
+                    Storage.StoredItemModifier.Insulate
+                });
+            }
+        }
+    }
+}
---
+++ BottleEmptierFixed.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace QualityOfLifeONI
+{
+    [HarmonyPatch(typeof(BottleEmptierConfig), nameof(BottleEmptierConfig.ConfigureBuildingTemplate))]
+    public static class BottleEmptierFixed
+    {
+        public static void Postfix(GameObject go)
+        {
+            Storage storage = go.GetComponent<Storage>();
+            if (storage != null)
+            {
+                storage.SetDefaultStoredItemModifiers(new List<Storage.StoredItemModifier>
+                {
+                    Storage.StoredItemModifier.Hide,
+                    Storage.StoredItemModifier.Seal,
+                    Storage.StoredItemModifier.Insulate 
+                });
+            }   
+        }
+    }
+
+    [HarmonyPatch(typeof(ManualGeneratorConfig), "CreateBuildingDef")]
+    class ManualGeneratorConfig_CreateBuildingDef
+    {
+        public static void Postfix(BuildingDef __result)
+        {
+            __result.GeneratorWattageRating = 600f;
+        }
+    }
+}
